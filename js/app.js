8f7bfe179b65d28d6fc1dfd4b3c82292dd1a511e
Ali H <34513931+ali-h@users.noreply.github.com>
2021-02-10 14:47:50 +0500

    deposit and withdraw functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const ACCOUNT = 'ali.dev';
+
 $(window).bind('load', async () => {
   await refreshUser();
   await stateManage();
@@ -52,4 +54,85 @@ $(document).on('click', '.pushBTN', async function(e) {
   e.preventDefault();
   const href = $(this).attr('href');
   await pushPage(href);
+});
+
+$(document).on('click', '#liquid_deposit', async function () {
+  const res = await ssc.findOne('tokens', 'balances', {
+    account: ACCOUNT,
+    symbol: 'SWAP.HIVE'
+  });
+
+  let liquidity = '0';
+  if (res) {
+    liquidity = parseFloat(res.balance);
+  }
+
+  $('#liquid_d').text(`${n(liquidity)} SWAP.HIVE`);
+});
+
+$(document).on('click', '#deposit', function () {
+  const quantity = $('#deposit_quantity').val();
+
+  if (BigNumber(quantity).gte(0.002)) {
+    if (!window.hive_keychain) toast('failure', 'Hive Keychain not installed');
+    else {
+      window.hive_keychain.requestTransfer(
+        user,
+        ACCOUNT,
+        BigNumber(quantity).toFixed(3),
+        '',
+        'HIVE',
+        function (response) {
+          if (response.error) {
+            toast('failure', 'An error occured, please try again');
+            return;
+          }
+
+          toast('success', `Deposit successfull, please wait a few seconds before refreshing`);
+          setTimeout(async function () {
+            location.reload();
+            return;
+          }, 6000);
+        },
+        true
+      );
+    }
+  }
+});
+
+$(document).on('click', '#withdraw', function () {
+  const quantity = $('#withdraw_quantity').val();
+
+  if (BigNumber(quantity).gte(0.002)) {
+    const obj = {
+      contractName: 'tokens',
+      contractAction: 'transfer',
+      contractPayload: {
+        to: ACCOUNT,
+        symbol: 'SWAP.HIVE',
+        quantity: BigNumber(quantity).toFixed(8)
+      }
+    };
+
+    if (!window.hive_keychain) toast('failure', 'Hive Keychain not installed');
+    else {
+      window.hive_keychain.requestCustomJson(
+        user,
+        'ssc-mainnet-hive',
+        'active',
+        JSON.stringify(obj),
+        'Withdraw SWAP.HIVE',
+        async function (response) {
+          if (response.error) {
+            toast('failure', 'An error occured, please try again');
+            return;
+          }
+
+          toast('success', `Withdraw successfull`);
+          await pushPage(location.href, response.result.id);
+          return;
+        }
+      );
+    }
+  }
 });
